84f5952cfaf64ca23ba95f97411e51cc0826bf93
ondrejzavadil <ondrej.zavadil@findly.com>
2015-11-05 22:21:27 +1300

    starting Marionette playground repo to play with marionette and try new things

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var browserify = require('gulp-browserify'),
+    del = require('del'),
+    gulp = require('gulp'),
+    gulpSequence = require('gulp-sequence');
+
+var path = {
+  scripts: ['js/**/*.js'],
+  build: 'build'
+};
+
+gulp.task('clean', function () {
+  return del(['build']);
+});
+
+gulp.task('scripts', function () {
+  gulp.src('js/**/*.js')
+    .pipe(browserify({
+      insertGlobals: true,
+      debug: !gulp.env.production
+    }))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(path.scripts, ['scripts']);
+});
+
+gulp.task('default', function (cb) {
+  gulpSequence('clean', 'scripts', 'watch', cb);
+});
---
+++ js/app.js
@@ -0,0 +1,5 @@
+var Marionette = require('backbone.marionette');
+
+var app =  Marionette.Application.extend({});
+
+console.log('ahoj');
